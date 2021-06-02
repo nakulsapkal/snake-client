0503804424b145190a83d137d6ab424fee21eaf9
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-06-02 23:59:58 +0000

    STDIN setup

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const { setInterval } = require("timers");
 
 // establishes a connection with the game server
 const connect = function() {
@@ -11,8 +12,26 @@ const connect = function() {
   conn.setEncoding("utf8");
 
   conn.on('connect', () => {
-    //console.log("Successfully connected to game server");
+    console.log("Successfully connected to game server");
     conn.write('Name: NAK');
+    // setTimeout(() => {
+    //   conn.write('Move: down');
+    //   setTimeout(() => {
+    //     conn.write('Move: left');
+    //     setTimeout(() => {
+    //       conn.write('Move: left');
+    //       setTimeout(() => {
+    //         conn.write('Move: right');
+    //         setTimeout(() => {
+    //           conn.write('Move: up');
+    //           setInterval(() => {
+    //             conn.write('Move: left');
+    //           }, 500);
+    //         }, 500);
+    //       }, 500);
+    //     }, 500);
+    //   }, 500);
+    // }, 500);
   });
 
   conn.on('data', (data) => {
