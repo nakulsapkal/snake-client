555ee82e8031a83db31b9ee55236bf46e5fe0feb
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-06-02 20:39:56 +0000

    Initial commit for files

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131', // IP address here,
+    port: 50542 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log("Connected");
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
