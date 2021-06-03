37de6662b123309a224691d26e986ef0add982a7
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-06-03 00:07:51 +0000

    input module setup

--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const handleUserInput = function(key) {
-  console.log(key);
-  if (key === '\u0003') {
-    process.exit();
-  } // your code here
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 console.log("Connecting ...");
 connect();
 setupInput();
